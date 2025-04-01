c17645778716f5e98dc8a61488875714d052900b
Douglas <douglas.luciano2@gmail.com>
2025-04-01 03:30:47 -0300

    Add database seeding for Regions and Walks

---
+++ Migrations/20250401062125_Seeding data for Difficulties and Regions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NZWalks.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingdataforDifficultiesandRegions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Difficulties",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("49079b20-86cd-4426-b27e-dc2924dd9656"), "Easy" },
+                    { new Guid("830919e8-9148-4360-b912-545056a9d131"), "Hard" },
+                    { new Guid("8b3100f2-99ed-4988-8baa-dda7486d96bb"), "Medium" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("49079b20-86cd-4426-b27e-dc2924dd9656"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("830919e8-9148-4360-b912-545056a9d131"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("8b3100f2-99ed-4988-8baa-dda7486d96bb"));
+        }
+    }
+}
