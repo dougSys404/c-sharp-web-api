9318c36a1cb13babb64d2639baaea8fd800d9d1c
Douglas <douglas.luciano2@gmail.com>
2025-04-10 13:24:56 -0300

    Creating controller for authentication, interface and repository for handling the tokens

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.Api.Model.DTO;
+
+namespace NZWalks.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        //Post Method: /api/Auth/Register
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+            };
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDto.Password);
+
+            if (identityResult.Succeeded)
+            {
+                // roles to this user
+                if (registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User was registered successfuly! Please login.");
+                    }
+                }
+            }
+
+            return BadRequest("Something went wrong!");
+        }
+
+        //Post: /api/Auth/Login
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        {
+            var user = await userManager.FindByEmailAsync(loginRequestDto.Username);
+
+            if (user != null)
+            {
+                var checkPassowrdResult = await userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+                
+                if (checkPassowrdResult)
+                {
+                    //create token
+                    return Ok();
+                }
+            }
+
+            return BadRequest("Username or password is incorrect");
+
+        }
+    }
+}
