70e4ba03c0f1025425a824b35388b051ef7c493f
Douglas <douglas.luciano2@gmail.com>
2025-04-14 23:57:04 -0300

    Creating GET request for Regions (GetAll) in web UI

---
+++ NzWalks.UI/Controllers/RegionsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NzWalks.UI.Models.DTO;
+using System.Threading.Tasks;
+
+namespace NzWalks.UI.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<RegionDto> response = new List<RegionDto>();
+
+            try
+            {
+                // get all region from web api
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.GetAsync("https://localhost:7121/api/regions");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
+
+                
+            }
+            catch (Exception ex)
+            {
+                //log the exception
+            
+            }
+
+            return View(response);
+        }
+    }
+}
--- NzWalks.UI/obj/Debug/net8.0/NzWalks.UI.AssemblyInfo.cs
+++ NzWalks.UI/obj/Debug/net8.0/NzWalks.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NzWalks.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77a9dcb74797aa3e3a2dce66d7e037f4d735c0fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("NzWalks.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NzWalks.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
