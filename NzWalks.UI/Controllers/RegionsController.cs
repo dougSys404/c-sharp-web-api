c6374d7fa678da935dfaf6a806bb07bac988bfb8
Douglas <douglas.luciano2@gmail.com>
2025-04-15 00:32:45 -0300

    Create Post Method for web UI Regions

--- NzWalks.UI/Controllers/RegionsController.cs
+++ NzWalks.UI/Controllers/RegionsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NzWalks.UI.Models;
 using NzWalks.UI.Models.DTO;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace NzWalks.UI.Controllers
@@ -37,6 +40,38 @@ namespace NzWalks.UI.Controllers
             }
 
             return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel model)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7121/api/regions"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage =  await client.SendAsync(httpRequestMessage);
+
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+
+            if (response is not null)
+            {
+                return RedirectToAction("Index", "Regions");
+            }
+
+            return View();
         }
     }
 }
